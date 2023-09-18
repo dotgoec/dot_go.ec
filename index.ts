a565972fdbe8e330fb4b16e63cb65eefbdc02d1b
Joe X! <jamedinao@gmail.com>
2023-09-18 16:20:15 -0500

    forgot a word

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ Bun.serve({
     fetch(req) {
       const url = new URL(req.url);
       const isindex = url.pathname.search(".") >= 0 ? "" : "index.html";
-      filepath = "public" + url.pathname + isindex;
+      const filepath = "public" + url.pathname + isindex;
       console.log(filepath);
       const file = Bun.file(filepath);
       if (file.size != 0) {
