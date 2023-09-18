352846a8ec2df7df4e1885e4bd35a26bf299ac08
Joe X! <jamedinao@gmail.com>
2023-09-18 15:15:18 -0500

    Website served

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+Bun.serve({
+    port: process.env.PORT,
+    hostname: process.env.IP,
+    fetch(req) {
+      return new Response(`
+        <html>
+        <head></head>
+        <body>Bun!<br>🤔</body>
+        </hhtml>`);
+    },
+  });
