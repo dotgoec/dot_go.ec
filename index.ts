61dae9ff5d2d0d6c026572e4122d354ba27f9113
Joe X! <jamedinao@gmail.com>
2023-09-18 16:12:46 -0500

    Testing redirecting to index.html

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ Bun.serve({
     hostname: process.env.IP,
     fetch(req) {
       const url = new URL(req.url);
-      const pathfile = Bun.file("public" + url.pathname);
-      if (pathfile.size != 0) return new Response(pathfile);
-      return new Response("404!");
+      const pathfile = Bun.file("public" + url.pathname + (url.pathname.search(".") >= 0 ? "" : "index.html"));
+      if (pathfile.size != 0) {
+        return new Response(pathfile);
+      } else {
+        return new Response("404!");
+      }
     },
   });
